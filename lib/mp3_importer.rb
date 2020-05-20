55379b942f71b73f65bc5b2eca7d0cd276e15938
michael thomas <michael.thomas2014@gmail.com>
2020-05-20 08:50:40 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,12 +6,12 @@ class MP3Importer
     @path = path
   end
   
-  def files(file)
-    dir[path]
+  def files
+    Dir[@path+"/*.mp3"].map { |file| file.split("/").last }
   end
-  
-  def import()
-    
+
+  def import
+    files.each { |file| Song.new_by_filename(file) }
   end
   
   
